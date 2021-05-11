8f84393c85015b5cb57a3bb6b75ec80d16cb509b
Jongho Lee <bazinga940@gmail.com>
2021-05-11 23:10:25 +0900

    layoutSubviews() 예제 함수 추가

--- iOS_Study/View Layout Test/ViewLayoutTestViewController.swift
+++ iOS_Study/View Layout Test/ViewLayoutTestViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class ViewLayoutTestViewController: UIViewController {
 	let testView = LayoutTestView(frame: CGRect(x: 0, y: 100, width: 200, height: 200))
+	let shapeView = ShapeView(frame: CGRect(x: 0, y: 500, width: 200, height: 200))
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		view.addSubview(testView)
+		view.addSubview(shapeView)
 //		testView.center = CGPoint(x: self.view.frame.size.width/2, y: 300)
 //		testView.contentMode = .redraw		// content mode 옵션을 redraw로 해주면 frame 변경 시 draw 코드가 자동으로 불린다.
 	}
@@ -21,6 +23,18 @@ class ViewLayoutTestViewController: UIViewController {
 		testView.setNeedsDisplay()		// setNeedsDisplay 호출 여부에 따라 선이 다르게 보인다. setNeedsDisplay 호출시 draw 코드 호출(즉시 호출은 아님 뷰의 드로잉 사이클에서 호출됨)
 		testView.frame.size.width = self.view.frame.size.width
 		testView.frame.size.height = self.view.frame.size.width
+
+//		shapeView.setNeedsLayout()
+		shapeView.frame.size.width = self.view.frame.size.width/2
+		shapeView.frame.size.height = self.view.frame.size.width/2
+	}
+}
+
+class ShapeView: UIView {
+
+	/// 처음 view의 크기가 변경 된 경우에만 호출됨
+	override func layoutSubviews() {
+		self.backgroundColor = .random()
 	}
 }
 
@@ -46,5 +60,16 @@ class LayoutTestView: UIView {
 		bpath.stroke()
 
 		print("draw has updated the view")
+	}
+}
+
+extension UIColor {
+	static func random() -> UIColor {
+		return UIColor(
+			red:   .random(in: 0...1),
+			green: .random(in: 0...1),
+			blue:  .random(in: 0...1),
+			alpha: 1.0
+		)
 	}
 }
