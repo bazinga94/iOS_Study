aa4abbf8de2317299e972f2743cedd181b61e1ac
이종호 <bazinga940@gmail.com>
2021-06-09 16:53:21 +0900

    KVO, swizzle 예제 코드 추가

---
+++ playground/swizzle.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class Thing : NSObject {
+	@objc dynamic
+	func original(x: Int) {
+		print("original called")
+	}
+}
+
+extension Thing {
+	@objc dynamic
+	func replacement(x: Int) {
+		print("replacement called")
+	}
+}
+
+func beforeAfter() {
+	let thing = Thing()
+	thing.original(x: 10) // calls original(x:)
+	let origMethod = #selector(Thing.original(x:))
+	let replacementMethod = #selector(Thing.replacement(x:))
+	let method: Method? = class_getInstanceMethod(Thing.self, origMethod)
+	let swizzleMethod: Method? = class_getInstanceMethod(Thing.self, replacementMethod)
+	let swizzleImpl = method_getImplementation(swizzleMethod!)
+	method_setImplementation(method!, swizzleImpl);
+	thing.original(x: 10) // calls replacement(x:)
+}
+
+beforeAfter()
+
+// 메소드 그 자체를 변경 할 수 있는 방법이 있다...!
+// https://developer.apple.com/documentation/objectivec/1418677-class_replacemethod
+// https://forums.swift.org/t/dynamic-method-replacement/16619
+// https://jcsoohwancho.github.io/2020-02-02-Selector%EC%99%80-Method-Swizzling/
+// https://zeddios.tistory.com/554
