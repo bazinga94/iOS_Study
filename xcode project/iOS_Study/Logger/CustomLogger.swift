863510c96fbab10642f1d3fe205dbf5c50197d1b
이종호 <bazinga940@gmail.com>
2021-06-11 09:20:35 +0900

    Logger 코드 완성

--- xcode project/iOS_Study/Logger/CustomLogger.swift
+++ xcode project/iOS_Study/Logger/CustomLogger.swift
@@ -9,29 +9,35 @@ import Foundation
 import os
 
 class CustomLogger {
+	/*
+	error: 오류를 일으킨 문제에 관한 메시지
+	warning: 아직 오류는 아니지만 발생할 수 있는 문제에 관한 메시지
+	info: 정보용 로그 메시지
+	debug: 개발 단계에서만 유용한 디버그 로그 메시지
+	verbose: 기본 로그 메시지(기본)
+	*/
 
 	// MARK: - 선택된 type에 해당하는 로그를 출력
-	private static func makeLogString(description: String, logType: String, tag: Any?, message: Any) -> String {
+	private static func makeLogString(description: String, tag: Any?, message: Any) -> String {
 
 //		let dateFormatter = DateFormatter()
 //		dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
 //		dateFormatter.timeZone = TimeZone.autoupdatingCurrent
 //		dateFormatter.locale = Locale.current
-//		let timestamp = dateFormatter.string(from: Date.init()) // 현재 시간
+//		let timestamp = dateFormatter.string(from: Date.init()) // 시간이 console에 나오기 때문에 로그에 추가 안함, 필요시 추가
 		let thread = Thread.current.isMainThread ? "main": Thread.current.name ?? "-" // 현재 스레드
 
 		if let tag = tag {
-//			return "\n" + description + "\n" + "[\(timestamp)] [\(thread)] [\(logType)] [Tag: \(tag)]\n>>> \(message)\n<<<\n"
-			return "\n" + description + "\n" + "[\(thread)] [Tag: \(tag)]\n>>> \(message)\n<<<\n"
+			return "\n" + description + "[Thread: \(thread)] [Tag: \(tag)]\n>>> \(message)\n<<<\n"
 		} else {
-			return "\n" + description + "\n" + "[\(thread)] \n>>>\n\(message)\n<<<\n"
+			return "\n" + description + "[Thread: \(thread)] \n>>>\n\(message)\n<<<\n"
 		}
 	}
 
 	static func error(fileName: String = #file, lineNumber: Int = #line, functionName: String = #function, tag: Any?, message: Any) {
-		var description: String = "[‼️] [\(sourceFileName(filePath: fileName))] \(functionName) [Line: \(lineNumber)]"
+		var description: String = "[‼️] \(sourceFileName(filePath: fileName)) [Line: \(lineNumber)] \(functionName) "
 
-		description = self.makeLogString(description: description, logType: "error", tag: tag, message: message)
+		description = self.makeLogString(description: description, tag: tag, message: message)
 		if #available(iOS 14.0, *) {
 			let logger = Logger()
 			logger.error("\(description)")
@@ -40,18 +46,60 @@ class CustomLogger {
 		} else {
 			print(description)
 		}
-//		fatalError(description)
+		fatalError(description)
+	}
+
+	static func warning(fileName: String = #file, lineNumber: Int = #line, functionName: String = #function, tag: Any?, message: Any) {
+		var description: String = "[⚠️] \(sourceFileName(filePath: fileName)) [Line: \(lineNumber)] \(functionName) "
+
+		description = self.makeLogString(description: description, tag: tag, message: message)
+		if #available(iOS 14.0, *) {
+			let logger = Logger()
+			logger.warning("\(description)")
+		} else if #available(iOS 10.0, *) {
+			os_log("%@", type: OSLogType.default, description)
+		} else {
+			print(description)
+		}
 	}
 
 	static func info(fileName: String = #file, lineNumber: Int = #line, functionName: String = #function, tag: Any?, message: Any) {
-		var description: String = "[‼️][\(sourceFileName(filePath: fileName))] [\(functionName)] [Line: \(lineNumber)]"
+		var description: String = "[ℹ️] \(sourceFileName(filePath: fileName)) [Line: \(lineNumber)] \(functionName) "
 
-		description = self.makeLogString(description: description, logType: "info", tag: tag, message: message)
+		description = self.makeLogString(description: description, tag: tag, message: message)
 		if #available(iOS 14.0, *) {
 			let logger = Logger()
 			logger.info("\(description)")
 		} else if #available(iOS 10.0, *) {
 			os_log("%@", type: .info, description)
+		} else {
+			print(description)
+		}
+	}
+
+	static func debug(fileName: String = #file, lineNumber: Int = #line, functionName: String = #function, tag: Any?, message: Any) {
+		var description: String = "[🐛] \(sourceFileName(filePath: fileName)) [Line: \(lineNumber)] \(functionName) "
+
+		description = self.makeLogString(description: description, tag: tag, message: message)
+		if #available(iOS 14.0, *) {
+			let logger = Logger()
+			logger.log("\(description)")
+		} else if #available(iOS 10.0, *) {
+			os_log("%@", type: .default, description)
+		} else {
+			print(description)
+		}
+	}
+
+	static func verbose(fileName: String = #file, lineNumber: Int = #line, functionName: String = #function, tag: Any?, message: Any) {
+		var description: String = "[💬] \(sourceFileName(filePath: fileName)) [Line: \(lineNumber)] \(functionName) "
+
+		description = self.makeLogString(description: description, tag: tag, message: message)
+		if #available(iOS 14.0, *) {
+			let logger = Logger()
+			logger.log("\(description)")
+		} else if #available(iOS 10.0, *) {
+			os_log("%@", type: .default, description)
 		} else {
 			print(description)
 		}
--- xcode project/iOS_Study/ViewController.swift
+++ xcode project/iOS_Study/ViewController.swift
@@ -11,8 +11,12 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		CustomLogger.error(tag: nil, message: "error 로그")
+
+		CustomLogger.warning(tag: nil, message: "warning 로그")
 		CustomLogger.info(tag: nil, message: "info 로그")
+//		CustomLogger.error(tag: nil, message: "error 로그")
+		CustomLogger.debug(tag: nil, message: "debug 로그")
+		CustomLogger.verbose(tag: nil, message: "verbose 로그")
 	}
 
 	@IBAction func setNeedsDisplayTest(_ sender: Any) {
