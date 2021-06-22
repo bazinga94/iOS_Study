3b260c15f47e019552b92261ea2225d54ad445cb
이종호 <bazinga940@gmail.com>
2021-06-22 09:33:19 +0900

    swizzle 예시 추가

--- xcode project/iOS_Study/ViewController.swift
+++ xcode project/iOS_Study/ViewController.swift
@@ -7,6 +7,24 @@
 
 import UIKit
 
+extension UIViewController {
+	class func swizzleMethod() {
+		let originalSelector = #selector(viewWillAppear)
+		let swizzleSelector = #selector(swizzleViewWillAppear)
+
+		guard
+			let originMethod = class_getInstanceMethod(UIViewController.self, originalSelector),
+			let swizzleMethod = class_getInstanceMethod(UIViewController.self, swizzleSelector)
+		else { return }
+
+		method_exchangeImplementations(originMethod, swizzleMethod)
+	}
+
+	@objc public func swizzleViewWillAppear(animated: Bool) {
+		print("swizzleViewWillAppear at \(String(describing: self))")
+	}
+}
+
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
@@ -17,6 +35,12 @@ class ViewController: UIViewController {
 //		CustomLogger.error(tag: nil, message: "error 로그")
 		CustomLogger.debug(tag: nil, message: "debug 로그")
 		CustomLogger.verbose(tag: nil, message: "verbose 로그")
+	}
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		print("original view will appear")
 	}
 
 	@IBAction func setNeedsDisplayTest(_ sender: Any) {
