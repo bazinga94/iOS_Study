f3bbd7727b4419df4510eb0f0aa755ced654fd25
Jongho Lee <bazinga940@gmail.com>
2021-07-02 12:44:06 +0900

    Observer Pattern 으로 Task 관리자 구현

---
+++ playground/ObserverPattern.playground/Contents.swift
@@ -0,0 +1,129 @@
+import Foundation
+
+/// Observer 등록 / 해제 protocol
+protocol Observable {
+	func addObserver(_ observer: Observer)
+	func removeObserver(_ observer: Observer)
+}
+
+/// Observer protocol
+protocol Observer: class {
+	var delegate: TaskManagerDelegate? { get set }
+	func taskStart()
+	func notifyFail()
+}
+
+/// Observer 관리 class
+class ObserverManager: Observable {
+	var observers: [Observer] = []
+
+	func addObserver(_ observer: Observer) {
+		observers.append(observer)
+	}
+
+	func removeObserver(_ observer: Observer) {
+		observers = observers.filter { $0 !== observer }		// ===, !== 클래스 주소값 비교!!
+	}
+}
+
+protocol TaskManagerDelegate: class {
+	func oneTaskCompleted(by observer: Observer)
+	func oneTaskFailed()
+}
+
+class SomeTaskManager: ObserverManager, TaskManagerDelegate {
+	/// 전체 task process 시작
+	func allTaskStart() {
+		print("All task start!")
+		observers.forEach {
+			$0.delegate = self
+			$0.taskStart()
+		}
+	}
+
+	/// 하나의 task 완료
+	func oneTaskCompleted(by observer: Observer) {
+		removeObserver(observer)
+		if observers.isEmpty {
+			allTaskCompleted()
+		}
+	}
+
+	/// 모든 task 완료
+	private func allTaskCompleted() {
+		print("All task completed!")
+	}
+
+	/// 하나의 task라도 실패하면 실패
+	func oneTaskFailed() {
+		observers.forEach {
+			$0.notifyFail()
+		}
+		print("Some task failed")
+	}
+}
+
+class Task1: Observer {
+	weak var delegate: TaskManagerDelegate?
+
+	func taskStart() {
+		print("\(String(describing: self)) start")
+		for i in (1...10) {
+			print("\(String(describing: self)) progressing \(10 * i)/100")
+			sleep(1)
+		}
+		print("\(String(describing: self)) end")
+		delegate?.oneTaskCompleted(by: self)
+//		delegate?.oneTaskFailed()
+	}
+
+	func notifyFail() {
+	}
+}
+
+class Task2: Observer {
+	weak var delegate: TaskManagerDelegate?
+
+	func taskStart() {
+		print("\(String(describing: self)) start")
+		for i in (1...5) {
+			print("\(String(describing: self)) progressing \(20 * i)/100")
+			sleep(1)
+		}
+		print("\(String(describing: self)) end")
+		delegate?.oneTaskCompleted(by: self)
+		//		delegate?.oneTaskFailed()
+	}
+
+	func notifyFail() {
+	}
+}
+
+class Task3: Observer {
+	weak var delegate: TaskManagerDelegate?
+
+	func taskStart() {
+		print("\(String(describing: self)) start")
+		for i in (1...4) {
+			print("\(String(describing: self)) progressing \(25 * i)/100")
+			sleep(1)
+		}
+		print("\(String(describing: self)) end")
+		delegate?.oneTaskCompleted(by: self)
+		//		delegate?.oneTaskFailed()
+	}
+
+	func notifyFail() {
+	}
+}
+
+let task1 = Task1()
+let task2 = Task2()
+let task3 = Task3()
+
+let someTaskManeger = SomeTaskManager()
+someTaskManeger.addObserver(task1)
+someTaskManeger.addObserver(task2)
+someTaskManeger.addObserver(task3)
+
+someTaskManeger.allTaskStart()
