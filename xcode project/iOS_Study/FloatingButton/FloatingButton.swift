0b1a794ba3cd0042f200ce8530b311c9ca9f9f90
이종호 <bazinga940@gmail.com>
2021-08-25 11:09:42 +0900

    Floating Button 추가

---
+++ xcode project/iOS_Study/FloatingButton/FloatingButton.swift
@@ -0,0 +1,19 @@
+//
+//  FloatingButton.swift
+//  iOS_Study
+//
+//  Created by Jongho Lee on 2021/08/25.
+//
+
+import UIKit
+
+class FloatingButton: UIView {
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+}
