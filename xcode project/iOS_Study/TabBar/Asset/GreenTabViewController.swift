7133e01b04061430723f35a8b4b4074f02f9da90
이종호 <bazinga940@gmail.com>
2021-08-27 11:54:17 +0900

    dragging 코드 작성

--- xcode project/iOS_Study/TabBar/Asset/GreenTabViewController.swift
+++ xcode project/iOS_Study/TabBar/Asset/GreenTabViewController.swift
@@ -28,9 +28,11 @@ class GreenTabViewController: UIViewController {
 	private let factory: GreenTabCellFactory = GreenTabCellFactory()
 //	private var cellControllers: [CellController<UICollectionView>] = []
 	private var cellControllers: [GenericCellController<GreenTabCollectionViewCell>] = []
-	private let cellWidth: CGFloat = 200
+//	private let cellWidth: CGFloat = 200
 	private let cellSpacing: CGFloat = 10		// cell 간격
 	private let horizontalInset: CGFloat = 30	// 컬렉션뷰 좌우 간격
+	private var isOneStepPaging = true
+	private var currentIndex: CGFloat = 0
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -57,6 +59,45 @@ extension GreenTabViewController: UICollectionViewDataSource {
 }
 
 extension GreenTabViewController: UICollectionViewDelegate {
+	func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
+	{
+
+		// item의 사이즈와 item 간의 간격 사이즈를 구해서 하나의 item 크기로 설정.
+//		let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+		let cellWidth = collectionView.bounds.width - horizontalInset * 2.0
+		let cellWidthIncludingSpacing = cellWidth + cellSpacing
+
+		// targetContentOff을 이용하여 x좌표가 얼마나 이동했는지 확인
+		// 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
+		var offset = targetContentOffset.pointee
+		let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+		var roundedIndex = round(index)
+
+		// scrollView, targetContentOffset의 좌표 값으로 스크롤 방향을 알 수 있다.
+		// index를 반올림하여 사용하면 item의 절반 사이즈만큼 스크롤을 해야 페이징이 된다.
+		// 스크로로 방향을 체크하여 올림,내림을 사용하면 좀 더 자연스러운 페이징 효과를 낼 수 있다.
+		if scrollView.contentOffset.x > targetContentOffset.pointee.x {
+			roundedIndex = floor(index)
+		} else if scrollView.contentOffset.x < targetContentOffset.pointee.x {
+			roundedIndex = ceil(index)
+		} else {
+			roundedIndex = round(index)
+		}
+
+		if isOneStepPaging {
+			if currentIndex > roundedIndex {
+				currentIndex -= 1
+				roundedIndex = currentIndex
+			} else if currentIndex < roundedIndex {
+				currentIndex += 1
+				roundedIndex = currentIndex
+			}
+		}
+
+		// 위 코드를 통해 페이징 될 좌표값을 targetContentOffset에 대입하면 된다.
+		offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
+		targetContentOffset.pointee = offset
+	}
 }
 
 extension GreenTabViewController: UICollectionViewDelegateFlowLayout {
