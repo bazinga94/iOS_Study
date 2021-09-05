5db617313b99aa4b78fbf640c64e9964a52efa82
이종호 <bazinga940@gmail.com>
2021-09-06 08:53:03 +0900

    주석 제거

--- xcode project/iOS_Study/UITableView/SampleTableViewController.swift
+++ xcode project/iOS_Study/UITableView/SampleTableViewController.swift
@@ -14,7 +14,6 @@ class SampleTableViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		tableView.register(UINib(nibName: "InnerTableViewCell", bundle: nil), forCellReuseIdentifier: "InnerTableViewCell")
-//		tableView.register(UINib(nibName: "TodayTableViewCell", bundle: nil), forCellReuseIdentifier: "TodayTableViewCell")
 		tableView.dataSource = self
 		tableView.delegate = self
 		tableView.rowHeight = UITableView.automaticDimension
@@ -28,8 +27,6 @@ extension SampleTableViewController: UITableViewDataSource {
 
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "InnerTableViewCell", for: indexPath)
-//		let cell = tableView.dequeueReusableCell(withIdentifier: "TodayTableViewCell", for: indexPath) as! TodayTableViewCell
-//		cell.titleLabel.text = "\(String(indexPath.row))"
 		return cell
 	}
 }
@@ -41,17 +38,13 @@ extension SampleTableViewController: UITableViewDelegate {
 		cell.alpha = 0
 		cell.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, 20, 0)
 
-//		UIView.animate(withDuration: 0.3, delay: 0.3 * Double(indexPath.row), options: .curveLinear) {
-//			cell.layer.transform = CATransform3DIdentity
-//			cell.alpha = 1
-//		}
-
 		DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 * Double(indexPath.row)) {
 			cell.alpha = 1
 			UIView.animate(withDuration: 0.3) {
 				cell.layer.transform = CATransform3DIdentity
 			}
 		}
+		// 이렇게 하면 문제점이 indexPath 에 따라 늦게 반응함...
 	}
 
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
