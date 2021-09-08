7c972899ab201d1944da37dee78c16d0a3bd2fb8
이종호 <bazinga940@gmail.com>
2021-09-08 09:32:48 +0900

    animation 구현중

--- xcode project/iOS_Study/UITableView/View/SampleTableViewController.swift
+++ xcode project/iOS_Study/UITableView/View/SampleTableViewController.swift
@@ -11,7 +11,8 @@ class SampleTableViewController: UIViewController {
 
 	private var viewModel: SampleTableViewModel = .init()
 	private var isCellAnimatedList: [IndexPath] = []
-	private var initialVisibleLastIndex: Int = 2	// 또 다른 방법!, 처음 보여질 셀에 대해 미리 알려주는것... 근데 이건 화면 크기 마다 다를텐데ㅠ.. 안되겠다
+	private var initialVisibleLastIndex: Int = 5	// 또 다른 방법!, 처음 보여질 셀에 대해 미리 알려주는것... 근데 이건 화면 크기 마다 다를텐데ㅠ.. 안되겠다
+	private var animationStart: Bool = false
 	private lazy var tableView: UITableView = UITableView()
 		.builder
 		.apply {
@@ -42,6 +43,8 @@ private extension SampleTableViewController {
 			DispatchQueue.main.async {
 				self?.isCellAnimatedList = []
 				self?.tableView.reloadData()
+//				self?.animationStart = true
+//				self?.initialVisibleLastIndex = self?.tableView.indexPathsForVisibleRows?.last?.row ?? 0
 			}
 		}
 	}
@@ -69,15 +72,20 @@ extension SampleTableViewController: UITableViewDelegate {
 
 	func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
 
+//		if !animationStart {
+//			return
+//		}
+
 //		print(tableView.indexPathsForVisibleRows)
+//		print(tableView.visibleCells)
 //		print(indexPath)
+
 		if indexPath.row > initialVisibleLastIndex {
 			if isCellAnimatedList.contains(indexPath) == false { //
 				cell.alpha = 0.05
 				let transform = CATransform3DTranslate(CATransform3DIdentity, 0, 30, 0)
 				cell.layer.transform = transform
 
-//				let delay = 0.3 * Double(indexPath.row)
 				let delay = 0.3
 
 				UIView.animate(withDuration: 1, delay: delay, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
@@ -94,7 +102,6 @@ extension SampleTableViewController: UITableViewDelegate {
 				cell.layer.transform = transform
 
 				let delay = 0.3 * Double(indexPath.row)
-//				let delay = 0.3
 
 				UIView.animate(withDuration: 1, delay: delay, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
 					cell.alpha = 1
