ff16672f87d081eb102942ddaa75464fc8111917
이종호 <bazinga940@gmail.com>
2021-09-09 14:11:06 +0900

    필요 없어진 ViewModel + 주석들 제거

--- xcode project/iOS_Study/UITableView/Cell/InnerTableViewCell.swift
+++ xcode project/iOS_Study/UITableView/Cell/InnerTableViewCell.swift
@@ -12,7 +12,6 @@ class InnerTableViewCell: UITableViewCell {
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var tableView: UITableView!
 
-//	var viewModel: InnerTableViewCellViewModel = .init(items: [])
 	var cellControllers: [TableCellController] = []
 
 	override func awakeFromNib() {
@@ -21,34 +20,23 @@ class InnerTableViewCell: UITableViewCell {
 		tableView.dataSource = self
 		tableView.delegate = self
 		tableView.estimatedRowHeight = 0
-//		viewModel.cellControllers.bind { [weak self] _ in
-//			DispatchQueue.main.async {
-//				self?.tableView.reloadData()
-//			}
-//		}
-		// Initialization code
 	}
 
 	override func setSelected(_ selected: Bool, animated: Bool) {
 		super.setSelected(selected, animated: animated)
-
-		// Configure the view for the selected state
 	}
 
 	override func prepareForReuse() {
 		super.prepareForReuse()
-//		self.viewModel.cellControllers.listener = nil	// 필요한가?
 	}
 }
 
 extension InnerTableViewCell: UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//		return viewModel.cellControllers.value.count
 		return cellControllers.count
 	}
 
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//		return viewModel.cellControllers.value[indexPath.row].cellFromReusableCellHolder(tableView, forIndexPath: indexPath)
 		return cellControllers[indexPath.row].cellFromReusableCellHolder(tableView, forIndexPath: indexPath)
 	}
 }
--- xcode project/iOS_Study/UITableView/View/SampleTableViewController.swift
+++ xcode project/iOS_Study/UITableView/View/SampleTableViewController.swift
@@ -11,7 +11,7 @@ class SampleTableViewController: UIViewController {
 
 	private var viewModel: SampleTableViewModel = .init()
 	private var isCellAnimatedList: [IndexPath] = []
-	private var initialVisibleLastIndex: Int = 3	// 또 다른 방법!, 처음 보여질 셀에 대해 미리 알려주는것... 근데 이건 화면 크기 마다 다를텐데ㅠ.. 안되겠다
+	private var initialVisibleLastIndex: Int = 0	// 또 다른 방법!, 처음 보여질 셀에 대해 미리 알려주는것... 근데 이건 화면 크기 마다 다를텐데ㅠ.. 안되겠다
 	private var animationStart: Bool = false
 	private var factory = SampleTableViewFactory()
 	private lazy var tableView: UITableView = UITableView()
@@ -46,9 +46,6 @@ private extension SampleTableViewController {
 			DispatchQueue.main.async {
 				self?.isCellAnimatedList = []
 				self?.tableView.reloadData()
-//				self?.animationStart = true
-//				self?.initialVisibleLastIndex = self?.tableView.indexPathsForVisibleRows?.last?.row ?? 0
-//				print("@@@@@", self?.tableView.indexPathsForVisibleRows)
 				self?.initialVisibleLastIndex = self?.tableView.indexPathsForVisibleRows?.last?.row ?? 0
 			}
 		}
@@ -76,14 +73,6 @@ extension SampleTableViewController: UITableViewDataSource {
 extension SampleTableViewController: UITableViewDelegate {
 
 	func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-
-//		if !animationStart {
-//			return
-//		}
-
-//		print(tableView.indexPathsForVisibleRows)
-//		print(tableView.visibleCells)
-//		print(indexPath)
 
 		if indexPath.row > initialVisibleLastIndex {
 			if isCellAnimatedList.contains(indexPath) == false { //
@@ -116,38 +105,6 @@ extension SampleTableViewController: UITableViewDelegate {
 				isCellAnimatedList.append(indexPath)
 			}
 		}
-
-//		if isCellAnimatedList.contains(indexPath) == false { //
-//			cell.alpha = 0.05
-//			let transform = CATransform3DTranslate(CATransform3DIdentity, 0, 30, 0)
-//			cell.layer.transform = transform
-//
-//			let delay = 0.3 * Double(indexPath.row)
-//			let delay = 0.3
-//
-//			UIView.animate(withDuration: 1, delay: delay, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-//				cell.alpha = 1
-//				cell.layer.transform = CATransform3DIdentity
-//			})
-//
-//			isCellAnimatedList.append(indexPath)
-//		}
-
-//		if !isCellAnimatedList.contains(indexPath) {
-//			print(indexPath)
-//			cell.alpha = 0
-//			cell.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, 20, 0)
-//
-//			DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 * Double((indexPath.row))) {
-//				cell.alpha = 1
-//				UIView.animate(withDuration: 0.3) {
-//					cell.layer.transform = CATransform3DIdentity
-////					cell.alpha = 1
-//				}
-//			}
-//			isCellAnimatedList.append(indexPath)
-//			// 이렇게 하면 문제점이 indexPath 에 따라 늦게 반응함...
-//		}
 	}
 
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
