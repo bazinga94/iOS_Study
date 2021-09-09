89410d050380d6b1a6f8efa4713dd5c6c80a286e
이종호 <bazinga940@gmail.com>
2021-09-09 10:20:31 +0900

    필요없는 코드 제거

--- xcode project/iOS_Study/UITableView/Cell/InnerTableViewCell.swift
+++ xcode project/iOS_Study/UITableView/Cell/InnerTableViewCell.swift
@@ -13,8 +13,6 @@ class InnerTableViewCell: UITableViewCell {
 	@IBOutlet weak var tableView: UITableView!
 
 	var viewModel: InnerTableViewCellViewModel = .init(items: [])
-//	private var isCellAnimatedList: [IndexPath] = []
-//	private var initialVisibleLastIndex: Int = 3
 
 	override func awakeFromNib() {
 		super.awakeFromNib()
@@ -24,9 +22,7 @@ class InnerTableViewCell: UITableViewCell {
 		tableView.estimatedRowHeight = 0
 		viewModel.cellControllers.bind { [weak self] _ in
 			DispatchQueue.main.async {
-//				self?.isCellAnimatedList = []
 				self?.tableView.reloadData()
-//				self?.initialVisibleLastIndex = self?.tableView.indexPathsForVisibleRows?.last?.row ?? 0
 			}
 		}
 		// Initialization code
@@ -38,11 +34,9 @@ class InnerTableViewCell: UITableViewCell {
 		// Configure the view for the selected state
 	}
 
-	override func layoutSubviews() {
-		super.layoutSubviews()
-		tableView.reloadData()
-		tableView.invalidateIntrinsicContentSize()
-		tableView.contentSize = tableView.intrinsicContentSize
+	override func prepareForReuse() {
+		super.prepareForReuse()
+		self.viewModel.cellControllers.listener = nil	// 필요한가?
 	}
 }
 
@@ -60,38 +54,4 @@ extension InnerTableViewCell: UITableViewDelegate {
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 		return 40
 	}
-
-//	func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//		if indexPath.row > initialVisibleLastIndex {
-//			if isCellAnimatedList.contains(indexPath) == false { //
-//				cell.alpha = 0.05
-//				let transform = CATransform3DTranslate(CATransform3DIdentity, 0, 20, 0)
-//				cell.layer.transform = transform
-//
-//				let delay = 0.2
-//
-//				UIView.animate(withDuration: 0.2, delay: delay, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-//					cell.alpha = 1
-//					cell.layer.transform = CATransform3DIdentity
-//				})
-//
-//				isCellAnimatedList.append(indexPath)
-//			}
-//		} else {
-//			if isCellAnimatedList.contains(indexPath) == false { //
-//				cell.alpha = 0.05
-//				let transform = CATransform3DTranslate(CATransform3DIdentity, 0, 20, 0)
-//				cell.layer.transform = transform
-//
-//				let delay = 0.2 * Double(indexPath.row)
-//
-//				UIView.animate(withDuration: 0.2, delay: delay, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-//					cell.alpha = 1
-//					cell.layer.transform = CATransform3DIdentity
-//				})
-//
-//				isCellAnimatedList.append(indexPath)
-//			}
-//		}
-//	}
 }
