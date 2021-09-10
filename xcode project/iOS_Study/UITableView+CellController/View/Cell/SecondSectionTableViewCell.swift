3398b5f0cb01ed3f0526ff578751e8abbc30f57f
이종호 <bazinga940@gmail.com>
2021-09-10 09:13:45 +0900

    cell constraint

--- xcode project/iOS_Study/UITableView+CellController/View/Cell/SecondSectionTableViewCell.swift
+++ xcode project/iOS_Study/UITableView+CellController/View/Cell/SecondSectionTableViewCell.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class SecondSectionTableViewCell: UITableViewCell {
 
+	enum Constant {
+		static let inset: CGFloat = 10
+		static let minimumLineSpacing: CGFloat = 10
+		static let minimumInteritemSpacing: CGFloat = 10
+		static let cellsPerRow = 5
+	}
+
 	@IBOutlet weak var collectionView: UICollectionView!
 
 	private let factory = SecondSectionTableViewCellFactory()
@@ -18,6 +25,7 @@ class SecondSectionTableViewCell: UITableViewCell {
 		super.awakeFromNib()
 		factory.registerCells(on: collectionView)
 		collectionView.dataSource = self
+		collectionView.delegate = self
 		// Initialization code
 	}
 
@@ -34,5 +42,25 @@ extension SecondSectionTableViewCell: UICollectionViewDataSource {
 
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		return cellControllers[indexPath.row].cellFromReusableCellHolder(collectionView, forIndexPath: indexPath)
+	}
+}
+
+extension SecondSectionTableViewCell: UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		return UIEdgeInsets(top: Constant.inset, left: Constant.inset, bottom: Constant.inset, right: Constant.inset)
+	}
+
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+		return Constant.minimumLineSpacing
+	}
+
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+		return Constant.minimumInteritemSpacing
+	}
+
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let marginsAndInsets = Constant.inset * 2 + collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right + Constant.minimumInteritemSpacing * CGFloat(Constant.cellsPerRow - 1)
+		let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(Constant.cellsPerRow)).rounded(.down)
+		return CGSize(width: itemWidth, height: itemWidth)
 	}
 }
