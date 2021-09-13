b17d884b1d16faba47b151713a682115ab8a08ed
이종호 <bazinga940@gmail.com>
2021-09-13 11:23:23 +0900

    샘플 컬렉션 뷰 그룹 생성

---
+++ xcode project/iOS_Study/UICollectionView+CellController/View/BaseCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseCollectionViewController.swift
+//  iOS_Study
+//
+//  Created by Jongho Lee on 2021/09/13.
+//
+
+import UIKit
+
+class BaseCollectionViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
